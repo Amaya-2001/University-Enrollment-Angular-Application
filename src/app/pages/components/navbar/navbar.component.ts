6ded3a1b7a9df6aecf50bc71ec6274ec348b445e
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-12-29 08:35:55 +0530

    set the logout btn if token exist

--- src/app/pages/components/navbar/navbar.component.ts
+++ src/app/pages/components/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+
+  }
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.signOut();
+  }
 
 }
